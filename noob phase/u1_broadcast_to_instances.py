0aa970b45377c14c55ec8c3418b034b8a41e5a61
mz3r0 <100568112+mz3r0@users.noreply.github.com>
2024-11-22 23:19:09 +0200

    Add files via upload

---
+++ noob phase/u1_broadcast_to_instances.py
@@ -0,0 +1,43 @@
+"""
+A experiment exploring how to broadcast property change to all instances of a
+widget.
+"""
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+
+class CustomButton(Button):
+    common_text = StringProperty()
+    _index = 0  # Just used to store an arb value
+    _instances = []
+
+    def __init__(self, **kwargs):
+        super(CustomButton, self).__init__(**kwargs)
+        CustomButton._instances.append(self)
+
+    @staticmethod
+    def on_common_text(instance, value):
+        print("called")
+        """ For each instance of this class, change its text """
+        for button in CustomButton._instances:
+            button.text = value
+            button.common_text = value
+
+    def on_release(self):
+        """ The button has been clicked. Set the common_text."""
+        self.common_text = "Click me! " + str(self._index)
+
+
+class TestApp(App):
+    def build(self):
+        layout = BoxLayout(orientation="vertical")
+        for i in range(0, 6):
+            but = CustomButton(text="Click me!")
+            but._index = i
+            layout.add_widget(but)
+        return layout
+
+if __name__ == "__main__":
+    TestApp().run()
